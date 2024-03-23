d93434d4e75d300d004123ae744e7fce15b23edb
BorislavStefanov31 <bstefanov@labforty.com>
2024-03-23 20:49:31 +0200

    upgrading text embedding

--- app/api/retrieval/process/docx/route.ts
+++ app/api/retrieval/process/docx/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
 
     if (embeddingsProvider === "openai") {
       const response = await openai.embeddings.create({
-        model: "text-embedding-3-small",
+        model: "text-embedding-3-large",
         input: chunks.map(chunk => chunk.content)
       })
 
