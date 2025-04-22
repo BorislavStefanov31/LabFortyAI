64bdff787277b847ebf728c1330691d0a3743fa6
BorislavStefanov31 <bstefanov@labforty.com>
2025-04-22 18:15:57 +0300

    Remove google models

--- lib/models/llm/google-llm-list.ts
+++ lib/models/llm/google-llm-list.ts
@@ -2,16 +2,4 @@ import { LLM } from "@/types"
 
 const GOOGLE_PLATORM_LINK = "https://ai.google.dev/"
 
-// Google Models (UPDATED 12/22/23) -----------------------------
-
-// Gemini Pro (UPDATED 12/22/23)
-const GEMINI_PRO: LLM = {
-  modelId: "gemini-pro",
-  modelName: "Gemini Pro",
-  provider: "google",
-  hostedId: "gemini-pro",
-  platformLink: GOOGLE_PLATORM_LINK,
-  imageInput: false
-}
-
-export const GOOGLE_LLM_LIST: LLM[] = [GEMINI_PRO]
+export const GOOGLE_LLM_LIST: LLM[] = []
